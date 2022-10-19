6dbcd9c41bd3b10a5567cb7225b211dd852b6408
Дмитрий Малашенко <dimaneopal@mail.ru>
2022-10-19 14:05:42 +0300

    7номер

--- Laba2/Program.cs
+++ Laba2/Program.cs
@@ -1,121 +1,121 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Laba2
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            N4();
-            Console.ReadLine();
-        }
+//namespace Laba2
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            N4();
+//            Console.ReadLine();
+//        }
 
-        public static void N1()
-        {
-            int r = 2;
+//        public static void N1()
+//        {
+//            int r = 2;
 
-            int x1 = 0;
-            int y1 = 2;
+//            int x1 = 0;
+//            int y1 = 2;
 
-            if (Math.Abs(x1 * x1 + y1 * y1 - r * r) <= Math.Pow(10, -3))
-            {
-                Console.WriteLine("точка 1 лежит ");
-            }
-            else
-            {
-                Console.WriteLine("точка 1 не лежит ");
-            }
+//            if (Math.Abs(x1 * x1 + y1 * y1 - r * r) <= Math.Pow(10, -3))
+//            {
+//                Console.WriteLine("точка 1 лежит ");
+//            }
+//            else
+//            {
+//                Console.WriteLine("точка 1 не лежит ");
+//            }
 
-            double x2 = 1.5;
-            double y2 = 0.7;
+//            double x2 = 1.5;
+//            double y2 = 0.7;
 
-            if (Math.Abs(x2 * x2 + y2 * y2 - r * r) <= Math.Pow(10, -3))
-            {
-                Console.WriteLine("точка 2 лежит ");
-            }
-            else
-            {
-                Console.WriteLine("точка 2 не лежит ");
-            }
+//            if (Math.Abs(x2 * x2 + y2 * y2 - r * r) <= Math.Pow(10, -3))
+//            {
+//                Console.WriteLine("точка 2 лежит ");
+//            }
+//            else
+//            {
+//                Console.WriteLine("точка 2 не лежит ");
+//            }
 
-            double x3 = 1;
-            double y3 = 1;
+//            double x3 = 1;
+//            double y3 = 1;
 
-            if (Math.Abs(x3 * x3 + y3 * y3 - r * r) <= Math.Pow(10, -3))
-            {
-                Console.WriteLine("точка 3 лежит ");
-            }
-            else
-            {
-                Console.WriteLine("точка 3 не лежит ");
-            }
+//            if (Math.Abs(x3 * x3 + y3 * y3 - r * r) <= Math.Pow(10, -3))
+//            {
+//                Console.WriteLine("точка 3 лежит ");
+//            }
+//            else
+//            {
+//                Console.WriteLine("точка 3 не лежит ");
+//            }
 
-            double x4 = 3;
-            double y4 = 0;
+//            double x4 = 3;
+//            double y4 = 0;
 
-            if (Math.Abs(x4 * x4 + y4 * y4 - r * r) <= Math.Pow(10, -3))
-            {
-                Console.WriteLine("точка 4 лежит ");
-            }
-            else
-            {
-                Console.WriteLine("точка 4 не лежит ");
-            }
+//            if (Math.Abs(x4 * x4 + y4 * y4 - r * r) <= Math.Pow(10, -3))
+//            {
+//                Console.WriteLine("точка 4 лежит ");
+//            }
+//            else
+//            {
+//                Console.WriteLine("точка 4 не лежит ");
+//            }
 
-        }
-        public static void N4()
-        {
-            Console.WriteLine("Введите число a");
-            double a = double.Parse(Console.ReadLine());
-            Console.WriteLine("Введите число b");
-            double b = double.Parse(Console.ReadLine());
-            Console.WriteLine("Введите число c ");
-            double c = double.Parse(Console.ReadLine());
+//        }
+//        public static void N4()
+//        {
+//            Console.WriteLine("Введите число a");
+//            double a = double.Parse(Console.ReadLine());
+//            Console.WriteLine("Введите число b");
+//            double b = double.Parse(Console.ReadLine());
+//            Console.WriteLine("Введите число c ");
+//            double c = double.Parse(Console.ReadLine());
 
 
-            double z = 0;
-            double res = 0;
-            if (a > b)
-            {
-                z = b;
-            }
-            else
-            {
-                z = a;
-            }
+//            double z = 0;
+//            double res = 0;
+//            if (a > b)
+//            {
+//                z = b;
+//            }
+//            else
+//            {
+//                z = a;
+//            }
 
-            if (z > c)
-            {
-                Console.WriteLine(z);
-            }
-            else
-            {
-                Console.WriteLine(c);
-            }
+//            if (z > c)
+//            {
+//                Console.WriteLine(z);
+//            }
+//            else
+//            {
+//                Console.WriteLine(c);
+//            }
 
             
-        }
+//        }
 
-        public static void N7()
-        {
-            Console.WriteLine("Введите число x");
+//        public static void N7()
+//        {
+//            Console.WriteLine("Введите число x");
 
-            double x = int.Parse(Console.ReadLine());
+//            double x = int.Parse(Console.ReadLine());
 
 
-            if (Math.Abs(x) > 1)
-            {
-                Console.WriteLine("y=1");
+//            if (Math.Abs(x) > 1)
+//            {
+//                Console.WriteLine("y=1");
 
-            }
-            else
-            {
-                Console.WriteLine("y= " + Math.Abs(x));
-            }
-        }
-    }
-}
+//            }
+//            else
+//            {
+//                Console.WriteLine("y= " + Math.Abs(x));
+//            }
+//        }
+//    }
+//}
